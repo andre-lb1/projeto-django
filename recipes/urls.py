21a05e72b2ee77683c33d1287bd1969359092752
André Luiz <andrelbor25@gmail.com>
2022-07-28 15:11:59 -0300

    removing contact and about

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,14 +1,12 @@
 from turtle import home
 from django.contrib import admin
-from django.http import HttpRequest
 from django.urls import path,include
-from .views import about,contact,home
+from .views import home
 
 #Domain.com/
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('about', about),
-    path('contact', contact),
+   
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/home.html')
 
-def about(request):
-    return HttpResponse('About.')
 
-def contact(request):
-    return render(request,'me-apague/home.html')
 
 
 
