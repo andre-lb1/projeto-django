f2953b70cb79c3e2e24f100d2177a11e5a5297a2
André Luiz <andrelbor25@gmail.com>
2022-08-02 17:18:17 -0300

    urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,9 @@ from django.urls import path,include
 from . import views
 
 #Domain.com/
-
+app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('recipes/<int:id>/', views.recipes, name='recipes'),
+    path('recipes/<int:id>/', views.recipes, name='recipe'),
    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from utils.recipes.random_recipes import make_recipe
 
 def home(request):
-    return render(request,'recipes/pages/home.html',{'name':'André Luiz'})
+    return render(request,'recipes/pages/home.html',{'recipes' : [make_recipe() for _ in range(10)]})
 
 def recipes(request,id):
-    return render(request,'recipes/pages/recipe-view.html')
+    return render(request,'recipes/pages/recipe-view.html',{'recipe' : make_recipe(),'is_detail_page':True})
 
 
 
