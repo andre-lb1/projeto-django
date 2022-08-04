21ba2b5459742e7c4c6e132d2304a10d63011cc0
André Luiz <andrelbor25@gmail.com>
2022-08-04 00:24:36 -0300

    raise errors, adjusting models

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,get_object_or_404
 from utils.recipes.random_recipes import make_recipe
-from .models import Recipes
+from .models import Recipes, Category
+from django.http import Http404
 
 def home(request):
     recipes = Recipes.objects.filter(is_published = True).order_by('-id')
     return render(request,'recipes/pages/home.html',{'recipes' : recipes})
 
 def category(request,category_id):
-    recipes = Recipes.objects.filter(category__id = category_id).order_by('-id')#__nome do campo do model de onde vem
+    # recipes = Recipes.objects.filter(category__id = category_id).order_by('-id')#__nome do campo do model de onde vem
     #a foreignkey
-    return render(request,'recipes/pages/category.html',{'recipes' : recipes})
-
+    recipes = get_list_or_404(Recipes.objects.filter(category_id = category_id, is_published = True).order_by('-id'))
+    return render(request,'recipes/pages/category.html',{'recipes' : recipes, 'category_name': f'Category - {recipes[0].category.name} │'})
+    
 def recipes(request,id):
     return render(request,'recipes/pages/recipe-view.html',{'recipe' : make_recipe(),'is_detail_page':True})
 
