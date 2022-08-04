b0bee1b75cf1ca1d27031f465ef8083bd34b0ee9
André Luiz <andrelbor25@gmail.com>
2022-08-04 18:32:18 -0300

    improving tests 1

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,5 @@
 from turtle import home
-
 from django.urls import include, path
-
 from . import views
 
 # Domain.com/
@@ -9,6 +7,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipes, name='recipe'),
-    path('recipes/category/<int:category_id>/',
-         views.category, name='category'),
+    path('recipes/category/<int:category_id>/', views.category, name='category'),
 ]
