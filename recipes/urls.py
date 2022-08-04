07381fd2176f34bd4ef82049e0ed1df167747249
André Luiz <andrelbor25@gmail.com>
2022-08-04 16:29:02 -0300

    raising tests

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,13 +1,14 @@
 from turtle import home
-from django.contrib import admin
-from django.urls import path,include
+
+from django.urls import include, path
+
 from . import views
 
-#Domain.com/
+# Domain.com/
 app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipes, name='recipe'),
-    path('recipes/category/<int:category_id>/', views.category, name='category'),
-   
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render,get_list_or_404,get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.random_recipes import make_recipe
-from .models import Recipes, Category
-from django.http import Http404
+from .models import Category, Recipes
+
 
 def home(request):
-    recipes = Recipes.objects.filter(is_published = True).order_by('-id')
-    return render(request,'recipes/pages/home.html',{'recipes' : recipes})
+    recipes = Recipes.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
 
-def category(request,category_id):
+
+def category(request, category_id):
     # recipes = Recipes.objects.filter(category__id = category_id).order_by('-id')#__nome do campo do model de onde vem
-    #a foreignkey
-    recipes = get_list_or_404(Recipes.objects.filter(category_id = category_id, is_published = True).order_by('-id'))
-    return render(request,'recipes/pages/category.html',{'recipes' : recipes, 'category_name': f'Category - {recipes[0].category.name} │'})
-    
-def recipes(request,id):
-    recipe = get_object_or_404(Recipes.objects.filter(id = id))
-    return render(request,'recipes/pages/recipe-view.html',{'recipe' : recipe,'is_detail_page':True})
+    # a foreignkey
+    recipes = get_list_or_404(Recipes.objects.filter(
+        category_id=category_id, is_published=True).order_by('-id'))
+    return render(request, 'recipes/pages/category.html', {'recipes': recipes, 'category_name': f'Category - {recipes[0].category.name} │'})
 
 
-
-
-
+def recipes(request, id):
+    recipe = get_object_or_404(Recipes.objects.filter(id=id))
+    return render(request, 'recipes/pages/recipe-view.html', {'recipe': recipe, 'is_detail_page': True})
